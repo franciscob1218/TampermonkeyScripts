3d69aadc380c82f237f7535091b3901e752e4376
franciscob1218 <56179130+franciscob1218@users.noreply.github.com>
2022-07-08 14:42:17 -0500

    Added functions for button and value changes

--- autofill.user.js
+++ autofill.user.js
@@ -7,20 +7,34 @@
 // ==/UserScript==
 
 //test push
-(function(){
 
-})
-
-document.addEventListener
+var lastname = document.getElementsByName("last_name");
 
 
-
-
-function changelastname(){
-
-    var lastname = document.getElementsByName("last_name");
+function changelastnamevalue(){
 
     lastname[0].value = "--"
+};
 
-}
+function dolastnamebutton() {
+    
+    let btn = document.createElement("button");
+    btn.innerHTML = "Click Me";
+    btn.addEventListener("click", changelastnamevalue)
+
+    var div_parent = lastname[0].parentElement;
+
+    div_parent.appendChild(btn);
+};
+
+var intervalID = window.setInterval(
+    dolastnamebutton,
+    2000
+);
+
+
+
+(function(){
+    dolastnamebutton();
+});
 

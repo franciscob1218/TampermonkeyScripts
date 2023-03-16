16589f392f302eade98cb87e80a08df52d3eb6d2
franciscob1218 <franciscob1218@gmail.com>
2023-03-15 22:42:17 -0500

    working version

--- uncheck.user.js
+++ uncheck.user.js
@@ -1,24 +1,17 @@
 // ==UserScript==
 // @name        Linkedin.uncheck
-// @version     0.2
+// @version     1.0
 // @include     https://www.linkedin.com/*
 // @namespace   https://greasyfork.org/users/932694
 // @description uncheck the follow box if active on easy apply
 // ==/UserScript==
 
-var checkbox = document.querySelector("follow-company-checkbox")
+var checkbox = document.getElementById("follow-company-checkbox");
 
-
-
-
-
-
-function run(){
-    
-    checkbox.checked;
-};
-
-(function(){
-    run();
-});
-
+// Set an interval to check the checkbox status every 0.5 seconds
+setInterval(function () {
+    // If the checkbox is checked, uncheck it
+    if (checkbox.checked) {
+        checkbox.checked = false;
+    }
+}, 500);
